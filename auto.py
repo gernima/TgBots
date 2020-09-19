f0eb865d93991e13556893035aabf36c01f9e024
Gernima <30156104m>
2020-09-19 13:17:00 +0300

    new proxies

--- auto.py
+++ auto.py
@@ -409,7 +409,7 @@ cur = db.cursor()
 bots_dir = "E:\Боты"
 days_acc_stay = 0
 coinomi_password = "wallet0159456"
-Proxy(1000)
+Proxy(500)
 # res_accs = get_completed_bots_from_folder()
 # print(res_accs)
 # RegBot(res_accs[0])
