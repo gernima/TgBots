fee9aa93065c73190266cbc56b5e8c59aec25332
Max <braer.maks@gmail.com>
2020-10-15 21:44:07 +0300

    delete comment

--- main.py
+++ main.py
@@ -43,19 +43,19 @@ lock = Lock()
 db = sqlite3.connect('Account.db')
 cur = db.cursor()
 dict_db = get_data_from_db()
-x = 1
-process = subprocess.Popen([sys.executable, "bot_V2.py", str(x), dict_db[x]["PHONE"], dict_db[x]["PASS"], dict_db[x]["API_ID"], dict_db[x]["API_HASH"], dict_db[x]["LITECOIN"], dict_db[x]["DEVICE"]])
-process.wait()
-time.sleep(2)
+# x = 1
+# process = subprocess.Popen([sys.executable, "bot_V2.py", str(x), dict_db[x]["PHONE"], dict_db[x]["PASS"], dict_db[x]["API_ID"], dict_db[x]["API_HASH"], dict_db[x]["LITECOIN"], dict_db[x]["DEVICE"]])
+# process.wait()
+# time.sleep(2)
 input("Нажми Enter чтобы запустить...")
 # from_n = 1
 # to_n = 20
-# from_n = int(argv[1]) - 1
-# to_n = int(argv[2])
-# print(f"Start bots from {from_n} to {to_n}")
-# for i in range(from_n, to_n):
-#     # print(i)
-#     if i + 1 in dict_db:
-#         thread = Thread(target=start_process, args=(i + 1,))
-#         thread.start()
-#         time.sleep(2)
+from_n = int(argv[1]) - 1
+to_n = int(argv[2])
+print(f"Start bots from {from_n} to {to_n}")
+for i in range(from_n, to_n):
+    # print(i)
+    if i + 1 in dict_db:
+        thread = Thread(target=start_process, args=(i + 1,))
+        thread.start()
+        time.sleep(2)
